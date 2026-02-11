6077f200d96bf3e8a12cbfc6c5e2841a6e733240
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2026-02-11 18:08:10 -0300

    style: install biome

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,17 @@
 export interface LatLng {
-	lat: number
-	lng: number
+  lat: number
+  lng: number
 }
 
 export interface Location extends LatLng {
-	address: string
+  address: string
 }
 
 export interface FavoriteLocation extends Location {
-	id: string
-	createdAt: number
+  id: string
+  createdAt: number
 }
 
 export interface MapViewState extends LatLng {
-	zoom: number
+  zoom: number
 }
