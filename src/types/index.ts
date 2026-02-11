38f0fbdd000701cc5ab5f38579adb483aa9df704
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2026-02-11 15:59:54 -0300

    chore: create project folder strucuture, initial types and biome

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface LatLng {
+	lat: number
+	lng: number
+}
+
+export interface Location extends LatLng {
+	address: string
+}
+
+export interface FavoriteLocation extends Location {
+	id: string
+	createdAt: number
+}
+
+export interface MapViewState extends LatLng {
+	zoom: number
+}
